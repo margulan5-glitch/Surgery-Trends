68a28c3cda7fff4ace748f8328b77afa38c1daf3
margulan5-glitch <musahanmargulan5@gmail.com>
2026-09-21 21:44:57 +0500

    Update Supabase URL and anon key in config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-window.SUPABASE_URL='https://YOUR-PROJECT.supabase.co';window.SUPABASE_ANON_KEY='YOUR_PUBLIC_ANON_KEY';
+window.SUPABASE_URL='https://uxuwqzhuncbyraayhpyl.supabase.co';window.SUPABASE_ANON_KEY='sb_publishable_Q1up5X4vrBef7Jko5YzPGA_QymxU7v6';
